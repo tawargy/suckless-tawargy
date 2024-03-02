070dbf9055d1bfe1ea9ae20c79c00e92a9237474
Mohamed El Tawargy <tawargy.dev@gmail.com>
2024-03-02 15:08:41 +0200

    dwm update

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,19 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	
-	{"","scripts/sb/sb-cpu", 5, 0},
-	{"","scripts/sb/sb-cpu-temp", 10,	0}, 
-  	{"","scripts/sb/sb-memory",10,0},
-    {"","scripts/sb/sb-brightness", 0, 17},
-    {"","scripts/sb/sb-up", 9000, 0},
-    {"","scripts/sb/sb-volume", 0, 10},
-    {"","scripts/sb/sb-internet", 15, 0},
-	{"","scripts/sb/sb-battery", 5, 3}, 
-    {"","scripts/sb/sb-key",5,13},
-    {"","scripts/sb/sb-clock",60,0}
+	{"M ", "/home/tawargy/bin/dwmblocks_scripts/memory",	     6,		              1},
+	{"🔊 ", "/home/tawargy/bin/dwmblocks_scripts/volume",     0,		              10},
+	{"📦 ", "/home/tawargy/bin/dwmblocks_scripts/update",     360, 		          9},
+	{"", "/home/tawargy/bin/dwmblocks_scripts/clock",	       60,	              0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " ";
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
+static char delim[] = "| ";
 static unsigned int delimLen = 5;
